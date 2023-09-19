5971d4aad973506f9f8541a60286e36c20e3a742
HamrounAdel <HamrounAdel@email.fr>
2023-09-20 00:37:33 +0100

    creation page

--- checkpoint/src/App.js
+++ checkpoint/src/App.js
@@ -77,15 +77,9 @@ function App() {
     <br></br>
     <br></br>
     <Card className="text-center">
-      <Card.Header>Featured</Card.Header>
-      <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      
+     
+      <Card.Footer className="text-muted"> footer   </Card.Footer>
     </Card>
     
   </div>
